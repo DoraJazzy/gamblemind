0e1d9459c1df33c4bf881ca0cc5a3c3d555a9c59
DoraJazzy <202409096@post.au.dk>
2025-03-20 16:29:44 +0100

    Base

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import About from './Components/About.js';
 import Option1 from './Components/Option1.js';
 import Option2 from './Components/Option2.js';
 import Option3 from './Components/Option3.js';
+import Footer from './Components/Footer.js';
 
 function App() {
-  let Component 
+  let Component; 
+
   switch(window.location.pathname) {
     case "/":
-      Component = App 
+      Component = About;
       break
       case "/about":
-        Component = About
+        Component = About;
         break
         case "/option1":
-          Component = Option1
+          Component = Option1;
           break
           case "/option2":
-            Component = Option2
+            Component = Option2;
             break
             case "/option3":
-              Component = Option3
+              Component = Option3;
               break
               default:
                 Component = () => <h1>404: Not Found</h1>
@@ -30,6 +32,7 @@ function App() {
   <div>
     <Navbar />
     <Component />
+    <Footer />
   </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./navbar.css";
+import "./Footer.css";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
